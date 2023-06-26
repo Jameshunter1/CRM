1e5bedc4c29afedd142e137d9d819d1f03f1789c
[James Hunter] <90528783+Jameshunter1@users.noreply.github.com>
2023-06-25 22:27:36 -0400

    Add delete record functionality

--- dcrm/website/views.py
+++ dcrm/website/views.py
@@ -47,3 +47,22 @@ def register_user(request):
 		return render(request, 'register.html', {'form':form})
 
 	return render(request, 'register.html', {'form':form})
+
+def customer_record(request, pk): 
+    if request.user.is_authenticated:
+	    #Look up records
+        customer_record = Record.objects.get(id=pk)
+        return render(request, 'record.html', {'customer_record':customer_record})
+    else:
+        messages.success(request, "You must be Logged In to view this page")
+        return redirect('home')
+
+def delete_record(request, pk):
+	if request.user.is_authenticated:
+		delete_it = Record.objects.get(id=pk)
+		delete_it.delete()
+		messages.success(request, "Record Deleted Successfully...")
+		return redirect('home')
+	else:
+		messages.success(request, "You Must Be Logged In To Do That...")
+		return redirect('home')
